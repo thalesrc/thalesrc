741bad8725e12a88c8e3c3de2c26c84c201ae297
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2024-09-28 18:21:24 +0300

    fix(extra-ts-types): fixed type

--- libs/extra-ts-types/src/deepest.type.ts
+++ libs/extra-ts-types/src/deepest.type.ts
@@ -6,5 +6,5 @@ export type DeepestValue<T, K extends keyof T> = T[K] extends Record<K, any> ? D
 /**
  * Get the deepest object of a nested object
  */
-export type DeepestObject<T, K extends keyof T> = T[K] extends Record<K, infer R> ? K extends keyof R ? DeepestObject<R, K> : R : T;
+export type DeepestObject<T, K extends keyof T> = T[K] extends Record<K, infer R> ? K extends keyof R ? DeepestObject<R, K> : R : never;
 
