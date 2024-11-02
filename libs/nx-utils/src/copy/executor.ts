fc001f64f4ca7d7dcaea7c2a82246b17a6ce1c7b
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2024-11-02 15:04:10 +0300

    fix(nx-utils): reverted to commonjs

--- libs/nx-utils/src/copy/executor.ts
+++ libs/nx-utils/src/copy/executor.ts
@@ -8,7 +8,7 @@ import { config, parse } from 'dotenv';
 import { glob } from 'glob';
 //@ts-expect-error this import shows error but works anyway
 import * as regexParser from 'regex-parser';
-import sharp from 'sharp';
+import * as sharp from 'sharp';
 
 import { ensureDirectory } from "@thalesrc/node-utils";
 
