b0822c766c74024fce2be81f92216d034c19e116
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2024-11-14 23:45:24 +0300

    fix(nx-utils): fixed not running parallel task

--- libs/nx-utils/src/run-parallel/executor.ts
+++ libs/nx-utils/src/run-parallel/executor.ts
@@ -1,9 +1,10 @@
 import { PromiseExecutor, logger } from '@nx/devkit';
 import { chain } from '@thalesrc/js-utils/promise/chain';
 import { tryCatch } from '@thalesrc/js-utils/promise/try-catch';
+import { arrayize } from '@thalesrc/js-utils/array/arrayize';
+import { never } from '@thalesrc/js-utils/promise/never';
 import { RunParallelExecutorSchema } from './schema';
 import { exec } from 'child_process';
-import { arrayize } from '@thalesrc/js-utils/array/arrayize';
 
 const runExecutor: PromiseExecutor<RunParallelExecutorSchema> = async (
   { commands, cwd: defaultCwd },
@@ -37,6 +38,10 @@ const runExecutor: PromiseExecutor<RunParallelExecutorSchema> = async (
     return {
       success: false,
     };
+  }
+
+  if (!commands.at(-1).readyWhen) {
+    await never();
   }
 
   return {
