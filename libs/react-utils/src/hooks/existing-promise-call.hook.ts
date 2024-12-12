e518b7dc4b7a6cfd90fe8c32d23a170c423da3cc
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2024-12-12 18:04:20 +0300

    fix(ReactUtils): fixed useExistingPromiseCall

--- libs/react-utils/src/hooks/existing-promise-call.hook.ts
+++ libs/react-utils/src/hooks/existing-promise-call.hook.ts
@@ -1,18 +1,20 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { useRef } from "react";
+import { useCallback, useRef } from "react";
 
 export function useExistingPromiseCall<T = any, S = any, F extends (this: S, ...args: any[]) => T = any>(
   func: F,
   thisArg: S | null = null,
-  args: Parameters<F> = [] as any): Promise<T> {
-  const ref = useRef<Promise<T>>(null);
-
-  (ref as any).current = ref.current || Promise.resolve().then(() => func.apply(thisArg!, args)).then(res => {
+  args: Parameters<F> = [] as any
+): () => Promise<T> {
+  const ref = useRef<() => Promise<T>>(null);
+  const caller = useCallback(() => Promise.resolve().then(() => func.apply(thisArg!, args)).then(res => {
     (ref as any).current = null;
 
     return res;
-  });
+  }), [func, thisArg, args]);
+
+  (ref as any).current = ref.current ?? caller;
 
   return ref.current!;
 }
