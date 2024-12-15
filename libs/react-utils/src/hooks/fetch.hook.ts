66013fab7e28d516b50f3e7e7f117239673221bc
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2024-12-15 22:10:04 +0300

    feat(ReactUtils): added params support to useFetch

--- libs/react-utils/src/hooks/fetch.hook.ts
+++ libs/react-utils/src/hooks/fetch.hook.ts
@@ -1,44 +1,99 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { useCallback } from 'react';
 
+/**
+ * The type of the body of a request.
+ */
 export type BodyType = BodyInit | {} | null | undefined;
+
+/**
+ * The type of the parameters of a request.
+ */
+export type ParamType = Record<string, string | number | boolean | null | undefined> | undefined;
+
+/**
+ * The type of the query parameters of a request.
+ */
 export type QueryType = Record<string, string> | undefined;
 
-export interface RequestType<R, B extends BodyType, Q extends QueryType> extends Omit<RequestInit, 'body'> {
+/**
+ * The type of a request configuration object.
+ */
+export interface RequestType<R, B extends BodyType, P extends ParamType, Q extends QueryType> extends Omit<RequestInit, 'body'> {
   query?: Q;
   body?: B;
+  params?: P;
   handleResponse?: (response: Response) => Promise<R>;
 }
 
+/**
+ * The default response handler for fetch requests.
+ * @param res - The response object.
+ * @returns The JSON representation of the response.
+ */
 export function baseHandleResponse(res: Response) {
   if (res.ok) return res.json();
 
   throw res;
 }
 
+/**
+ * A custom hook for making HTTP requests with fetch.
+ *
+ * @param url - The URL to which the request is sent.
+ * @param request - The request configuration object.
+ * @param deps - The dependency array for the useCallback hook.
+ *
+ * @returns A function that makes the HTTP request when called.
+ *
+ * @example
+ * const fetchData = useFetch('https://api.example.com/data/{id}', {
+ *   method: 'GET',
+ *   params: { id: 123 },
+ *   query: { search: 'foo' },
+ * });
+ *
+ * fetchData().then(response => {
+ *   console.log(response);
+ * }).catch(error => {
+ *   console.error(error);
+ * });
+ */
 export function useFetch<
   Res = any,
   Body extends BodyType = undefined,
+  Params extends ParamType = undefined,
   Query extends QueryType = undefined,
 >(
   url: string,
-  request: RequestType<Res, Body, Query> = {},
+  request: RequestType<Res, Body, Params, Query> = {},
   deps: any[] = [url],
 ) {
 
   return useCallback(
     ({
-      query,
       handleResponse = baseHandleResponse,
       body = request.body,
+      params = request.params,
+      query = request.query,
       ...overrides
-    }: RequestType<Res, Body, Query> = {}) => {
+    }: RequestType<Res, Body, Params, Query> = {}) => {
       if (![Blob, ArrayBuffer, FormData, URLSearchParams].some(type => body instanceof type)) {
         body = JSON.stringify(body) as Body;
       }
 
+      const parsedUrl = url.replace(/{(\w+)}/g, (_, key) => {
+        const value = params?.[key];
+
+        if (value === undefined) {
+          throw new Error(`Missing value for parameter ${key}`);
+        }
+
+        return encodeURIComponent(String(value));
+      });
+
       return fetch(
-        `${url}${query ? '?' : ''}${new URLSearchParams(query).toString()}`,
+        `${parsedUrl}${query ? '?' : ''}${new URLSearchParams(query).toString()}`,
         {
           headers: new Headers({ Accept: 'application/json', 'Content-Type': 'application/json' }),
           ...request,
