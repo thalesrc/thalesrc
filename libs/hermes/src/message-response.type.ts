ee1b65844e435dd2590623c305cfa9f4737d0c27
Ali Şahin Özçelik <alisahinozcelik@gmail.com>
2025-12-19 17:59:47 +0300

    refactor(hermes): simplify ESLint configuration and enhance error message type

--- libs/hermes/src/message-response.type.ts
+++ libs/hermes/src/message-response.type.ts
@@ -5,7 +5,7 @@ type Omit<ObjectType, KeysType extends keyof ObjectType> = Pick<ObjectType, Excl
 export type UncompletedMessageResponse<T = any> = Omit<Message<T>, 'path'> & { completed: false; };
 type CompletedMessageResponse = Omit<Message, 'body' | 'path'> & { completed: true; };
 
-export type ErrorMessageResponse = {error: any};
+export type ErrorMessageResponse = {error: any; id: string; completed: true; };
 export type SuccessfulMessageResponse<T = any> = UncompletedMessageResponse<T> | CompletedMessageResponse;
 
 export type MessageResponse<T = any> = SuccessfulMessageResponse<T> | ErrorMessageResponse;
